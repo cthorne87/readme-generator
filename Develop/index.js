f3d1ccaa591d7b9db8396f4781c3736ffecceee1
Chris Thorne <u1400900@umail.utah.edu>
2022-01-06 16:25:50 -0700

    some-questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,30 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require(".generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What's your project's title?",
+    },
+    {
+        type: "editor",
+        name: "Description",
+        message: "Describe your project.",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "What command should be used to install your app?",
+    },
+    {
+        type: "editor",
+        name: "Usage"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
